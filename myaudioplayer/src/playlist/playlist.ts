70b2b1904b09dcf47abb506b11602303e4b2a8bd
bdino673@gmail.com <bdino673@gmail.com>
2024-04-05 14:58:31 +0100

    Updates

--- myaudioplayer/src/playlist/playlist.ts
+++ myaudioplayer/src/playlist/playlist.ts
@@ -2,10 +2,42 @@ import trackOne from './songs/track1.mp3';
 import trackTwo from './songs/track2.mp3';
 import trackThree from './songs/track3.mp3';
 import trackFour from './songs/track4.mp3';
+import trackFive from './songs/track5.mp3';
+import trackSix from './songs/track6.mp3';
+import trackSeven from './songs/track7.mp3';
+import trackEight from './songs/track8.mp3';
+import trackNine from './songs/track9.mp3';
+import trackTen from './songs/track10.mp3';
+import trackEleven from './songs/track11.mp3';
+import trackTwelve from './songs/track12.mp3';
+import trackThirteen from './songs/track13.mp3';
+import trackFourteen from './songs/track14.mp3';
+import trackFifteen from './songs/track15.mp3';
+import trackSixteen from './songs/track16.mp3';
+import trackSeventeen from './songs/track17.mp3';
+import trackEighteen from './songs/track18.mp3';
+import trackNineteen from './songs/track19.mp3';
+import trackTwenty from './songs/track20.mp3';
 import coverArtOne from './cover-art/1.jpg';
 import coverArtTwo from './cover-art/2.jpg';
 import coverArtThree from './cover-art/3.jpg';
 import coverArtFour from './cover-art/4.jpg';
+import coverArtFive from './cover-art/5.jpg';
+import coverArtSix from './cover-art/6.jpg';
+import coverArtSeven from './cover-art/7.jpg';
+import coverArtEight from './cover-art/8.jpg';
+import coverArtNine from './cover-art/9.jpg';
+import coverArtTen from './cover-art/10.jpg';
+import coverArtEleven from './cover-art/11.jpg';
+import coverArtTwelve from './cover-art/12.jpg';
+import coverArtThirteen from './cover-art/13.jpg';
+import coverArtFourteen from './cover-art/14.jpg';
+import coverArtFifteen from './cover-art/15.jpg';
+import coverArtSixteen from './cover-art/16.jpg';
+import coverArtSeventeen from './cover-art/17.jpg';
+import coverArtEighteen from './cover-art/18.jpg';
+import coverArtNineteen from './cover-art/19.jpg';
+import coverArtTwenty from './cover-art/20.jpg';
 import { Playlist } from '../audioplayer/types';
 
 const playlist: Playlist = [
@@ -37,8 +69,136 @@ const playlist: Playlist = [
     audioSrc: trackFour,
     metadata: {
       title: 'Flowers',
-      artist: 'Jane Doe',
+      artist: '',
       coverArtSrc: coverArtFour,
+    },
+  },
+  {
+    audioSrc: trackFive,
+    metadata: {
+      title: 'victony',
+      artist: 'Dino',
+      coverArtSrc: coverArtFive,
+    },
+  },
+  {
+    audioSrc: trackSix,
+    metadata: {
+      title: 'victony',
+      artist: 'Dino',
+      coverArtSrc: coverArtSix,
+    },
+  },
+  {
+    audioSrc: trackSeven,
+    metadata: {
+      title: 'victony',
+      artist: 'Dino',
+      coverArtSrc: coverArtSeven,
+    },
+  },
+  {
+    audioSrc: trackEight,
+    metadata: {
+      title: 'victony',
+      artist: 'Dino',
+      coverArtSrc: coverArtEight,
+    },
+  },
+  {
+    audioSrc: trackNine,
+    metadata: {
+      title: 'Rexxie',
+      artist: 'Dino',
+      coverArtSrc: coverArtNine,
+    },
+  },
+  {
+    audioSrc: trackTen,
+    metadata: {
+      title: 'DJ-Neptune',
+      artist: 'Dino',
+      coverArtSrc: coverArtTen,
+    },
+  },
+  {
+    audioSrc: trackEleven,
+    metadata: {
+      title: 'Dj-Big',
+      artist: 'Dino',
+      coverArtSrc: coverArtEleven,
+    },
+  },
+  {
+    audioSrc: trackTwelve,
+    metadata: {
+      title: 'crayon',
+      artist: 'Dino',
+      coverArtSrc: coverArtTwelve,
+    },
+  },
+  {
+    audioSrc: trackThirteen,
+    metadata: {
+      title: 'Ckay',
+      artist: 'Dino',
+      coverArtSrc: coverArtThirteen,
+    },
+  },
+  {
+    audioSrc: trackFourteen,
+    metadata: {
+      title: 'central',
+      artist: 'Dino',
+      coverArtSrc: coverArtFourteen,
+    },
+  },
+  {
+    audioSrc: trackFifteen,
+    metadata: {
+      title: 'carlos',
+      artist: 'Dino',
+      coverArtSrc: coverArtFifteen,
+    },
+  },
+  {
+    audioSrc: trackSixteen,
+    metadata: {
+      title: 'brown',
+      artist: 'Dino',
+      coverArtSrc: coverArtSixteen,
+    },
+  },
+  {
+    audioSrc: trackSeventeen,
+    metadata: {
+      title: 'boy-spyc',
+      artist: 'Dino',
+      coverArtSrc: coverArtSeventeen,
+    },
+  },
+  {
+    audioSrc: trackEighteen,
+    metadata: {
+      title: 'bobby',
+      artist: 'Dino',
+      coverArtSrc: coverArtEighteen,
+    },
+  },
+  {
+    audioSrc: trackNineteen,
+    metadata: {
+      title: 'Av',
+      artist: 'Dino',
+      coverArtSrc: coverArtNineteen,
+    },
+  },
+  {
+    audioSrc: trackTwenty,
+    metadata: {
+      title: 'Av',
+      artist: 'Dino',
+      coverArtSrc: coverArtTwenty,
     },
   },
 ];
